b2d4fee5d801828f5fa0bac6775edb5b15100e9a
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-21 13:38:16 -0500

    added resource endpoints, get and posts new

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,21 @@
 // build your `Resource` model here
+const ree = require('../../data/dbConfig.js');
+
+function getAll() {
+  return ree('resources');
+}
+
+function getById(id) {
+  return ree('resources').where('resource_id', id).first();
+}
+//create some resources!
+async function create(resource) {
+  const [id] = await ree('resources').insert(resource);
+  return getById(id);
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
+};
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,13 +1,27 @@
 // build your `/api/resources` router here
-const router = require('express').Router()
+const Resource = require('./model');
+const router = require('express').Router();
 
-router.use((err,req, res, next) => { // eslint-disable-line
-    res.status(500).json({
-        customMessage: 'ruh roh, something went wrong inside of the resource router.js file',
-        message: err.message,
-        stack: err.stack
-    })
-})
+//get everything from resource table - id, name, description
+router.get('/', (req, res) => {
+	Resource.getAll()
+		.then((resources) => {
+			res.json(resources);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'failed to retrieve resources', error: err.message });
+		});
+});
 
+//post a new resource
+router.post('/', (req, res) => {
+	Resource.create(req.body)
+		.then((newResource) => {
+			res.status(201).json(newResource);
+		})
+		.catch((err) => {
+			res.status(500).json({ message: 'Oops! Failed to create new resource!', error: err.message });
+		});
+});
 
-module.exports = router
+module.exports = router;

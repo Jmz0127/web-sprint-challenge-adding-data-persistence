2cffa5270cc95566febefcdbf26eeebff825c16e
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-21 11:38:34 -0500

    server is up and running

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,13 @@
 // build your `/api/projects` router here
+const router = require('express').Router()
+
+router.use((err,req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        customMessage: 'ruh roh, something went wrong inside of the project router.js file',
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+
+module.exports = router
